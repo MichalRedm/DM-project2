fed301ab24e96647772c5e8700e4f675b11bdfc3
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2024-05-22 23:55:34 +0200

    loading data - initial work

--- dm_project2/load_data.py
+++ dm_project2/load_data.py
@@ -1,4 +1,14 @@
+import os
 import pandas as pd
 
-def load_data() -> pd.DataFrame:
-    return pd.DataFrame()
+
+def load_data(dataset: str = 'ml-latest-small') -> pd.DataFrame:
+
+    dirname = os.path.dirname(__file__)
+
+    links = pd.read_csv(os.path.join(dirname, f'../data/raw/{dataset}/links.csv'))
+    movies = pd.read_csv(os.path.join(dirname, f'../data/raw/{dataset}/movies.csv'))
+    ratings = pd.read_csv(os.path.join(dirname, f'../data/raw/{dataset}/ratings.csv'))
+    tags = pd.read_csv(os.path.join(dirname, f'../data/raw/{dataset}/tags.csv'))
+
+    return ratings
