e322bf3cde55761847f68beaad0498088cd7b3df
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2024-05-22 14:27:02 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,18 @@
+from setuptools import setup, find_packages
 
+setup(
+  name='dm_project2',
+  version='0.1.0',
+  packages=find_packages(),
+  install_requires=[
+    'pandas',
+    'numpy',
+    'scikit-learn',
+    'matplotlib'
+  ],
+  entry_points={
+    'console_scripts': [
+      'dm_project2=dm_project2:main'
+    ]
+  }
+)
