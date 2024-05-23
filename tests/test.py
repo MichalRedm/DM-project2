00dff4252c7e2953711d7c2135ee223746051bf4
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2024-05-23 09:25:40 +0200

    tab = 4 spaces

--- tests/test.py
+++ tests/test.py
@@ -2,8 +2,8 @@ import unittest
 
 class TestStuff(unittest.TestCase):
   
-  def test_something(self):
-    pass
+    def test_something(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
