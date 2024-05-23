3f17fd08d8168495262f08d3769a0c52d6ebec39
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2024-05-23 09:57:31 +0200

    Update test_dataset.py

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,10 +1,12 @@
 import unittest
 import dm_project2.dataset as ds
 
+
 class TestDataset(unittest.TestCase):
   
     def test_invalid_dataset(self):
         self.assertRaises(ds.InvalidDatasetException, ds.MovieLensDataset, "lorem ipsum")
+
 
 if __name__ == '__main__':
     unittest.main()
