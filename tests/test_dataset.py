27ab9f2b64339a966ee9f46a0baf3f16acf5e61d
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2024-05-24 22:23:18 +0200

    TestDataset improved

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -4,14 +4,15 @@ import pandas as pd
 from context import dataset as ds
 
 
-def mock_init(cls):
-    cls._ratings = pd.DataFrame({
+def mock_init(self: ds.MovieLensDataset) -> None:
+    """Mock initialization for the class `MovieLensDataset`."""
+    self._ratings = pd.DataFrame({
         'userId': [1],
         'movieId': [1],
         'rating': [1],
         'timestamp': [0]
     }).set_index(['userId', 'movieId'])
-    cls._movies = pd.DataFrame({
+    self._movies = pd.DataFrame({
         'movieId': [1],
         'title': ['Lorem'],
         'genres': ['Ipsum']
@@ -19,11 +20,14 @@ def mock_init(cls):
 
 
 class TestDataset(unittest.TestCase):
+    """Set of test cases for the class `MovieLensDataset`."""
   
     def test_invalid_dataset(self):
+        """Check if an exception is thrown when incorrect dataset name is specified."""
         self.assertRaises(ds.InvalidDatasetException, ds.MovieLensDataset, "lorem ipsum")
     
     def test_get_movie_by_id(self):
+        """Check if a movie can be correctly retrieved by an id."""
         with mock.patch.object(ds.MovieLensDataset, '__init__', mock_init):
             dataset = ds.MovieLensDataset()
             movie = dataset.get_movie_by_id(1)
@@ -32,22 +36,26 @@ class TestDataset(unittest.TestCase):
             self.assertEqual(['Ipsum'], movie.genres)
 
     def test_get_nonexistent_movie(self):
+        """Check if attempting to retrieve a nonexistent movie will raise an exception."""
         with mock.patch.object(ds.MovieLensDataset, '__init__', mock_init):
             dataset = ds.MovieLensDataset()
             self.assertRaises(ds.InvalidMovieException, dataset.get_movie_by_id, 2)
     
     def test_delete_rating(self):
+        """Check if deleting rating works correctly."""
         with mock.patch.object(ds.MovieLensDataset, '__init__', mock_init):
             dataset = ds.MovieLensDataset()
             self.assertTrue(dataset.delete_rating(1, 1))
             self.assertTrue(dataset._ratings.empty)
     
     def test_delete_rating_for_nonexistent_user(self):
+        """Check if deleting rating from a user that does not exist will raise an exception."""
         with mock.patch.object(ds.MovieLensDataset, '__init__', mock_init):
             dataset = ds.MovieLensDataset()
             self.assertRaises(ds.InvalidUserException, dataset.delete_rating, 2, 1)
     
     def test_delete_rating_for_nonexistent_movie(self):
+        """Check if deleting rating for a movie that does not exist will raise an exception."""
         with mock.patch.object(ds.MovieLensDataset, '__init__', mock_init):
             dataset = ds.MovieLensDataset()
             self.assertRaises(ds.InvalidMovieException, dataset.delete_rating, 1, 2)
