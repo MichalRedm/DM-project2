bbc7388053fe443d8faa1657d11f847b3c409663
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2024-05-24 22:39:48 +0200

    Two more unit tests for MovieLensDataset

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -4,23 +4,40 @@ import pandas as pd
 from context import dataset as ds
 
 
+ratings_df = pd.DataFrame({
+    'userId': [1],
+    'movieId': [1],
+    'rating': [1],
+    'timestamp': [0]
+}).set_index(['userId', 'movieId'])
+
+movies_df = pd.DataFrame({
+    'movieId': [1],
+    'title': ['Lorem'],
+    'genres': ['Ipsum']
+}).set_index('movieId')
+
+
 def mock_init(self: ds.MovieLensDataset) -> None:
     """Mock initialization for the class `MovieLensDataset`."""
-    self._ratings = pd.DataFrame({
-        'userId': [1],
-        'movieId': [1],
-        'rating': [1],
-        'timestamp': [0]
-    }).set_index(['userId', 'movieId'])
-    self._movies = pd.DataFrame({
-        'movieId': [1],
-        'title': ['Lorem'],
-        'genres': ['Ipsum']
-    }).set_index('movieId')
+    self._ratings = ratings_df.copy()
+    self._movies = movies_df.copy()
 
 
 class TestDataset(unittest.TestCase):
     """Set of test cases for the class `MovieLensDataset`."""
+
+    def test_get_ratings(self):
+        """Check if retrieving the `ratings` data frame works properly."""
+        with mock.patch.object(ds.MovieLensDataset, '__init__', mock_init):
+            dataset = ds.MovieLensDataset()
+            self.assertTrue(ratings_df.equals(dataset.get_ratings()))
+    
+    def test_get_movies(self):
+        """Check if retrieving the `movies` data frame works properly."""
+        with mock.patch.object(ds.MovieLensDataset, '__init__', mock_init):
+            dataset = ds.MovieLensDataset()
+            self.assertTrue(movies_df.equals(dataset.get_movies()))
   
     def test_invalid_dataset(self):
         """Check if an exception is thrown when incorrect dataset name is specified."""
