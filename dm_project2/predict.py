5ea74a8f261a540eab36b391f45d2002101e67ee
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2024-05-26 21:24:55 +0200

    Basic prediction

--- dm_project2/predict.py
+++ dm_project2/predict.py
@@ -4,6 +4,7 @@ from typing import List
 from dataset import MovieLensDataset, InvalidMovieException, InvalidUserException, InvalidDatasetException
 from preprocessing import Preprocessing
 from clustering import Clustering
+from math import isnan
 
 logging.basicConfig(
     level=logging.INFO,
@@ -28,9 +29,12 @@ def predict(dataset_name: str, user_id: int, movie_id: int) -> float:
 
     similar_movies = clustering.get_movie_cluster(movie_id)
     
-    logger.info(f'Similar movies: {similar_movies}')
+    ratings = dataset.get_ratings()
+    return round_rating(ratings[ratings.index.get_level_values('userId') == user_id & ratings.index.get_level_values('movieId').isin(similar_movies)]['rating'].mean())
 
-    return 0.0
+
+def round_rating(rating: float) -> float:
+    return 3.0 if isnan(rating) else round(rating * 2) / 2
 
 
 def main(args: List[str]) -> None:
