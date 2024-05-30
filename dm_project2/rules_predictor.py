da5a1756d212945d7d7056ec08814857a02795b6
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2024-05-30 08:39:53 +0200

    Update rules_predictor.py

--- dm_project2/rules_predictor.py
+++ dm_project2/rules_predictor.py
@@ -5,13 +5,22 @@ from mlxtend.frequent_patterns import fpgrowth, association_rules
 from sklearn.metrics import mean_squared_error
 
 
+def baseline_predictor(user_id, movie_id, dataset_name = 'ml-latest-small') -> float:
+    dataset = MovieLensDataset(dataset_name)
+    dataset.delete_rating(user_id, movie_id)
+    ratings = dataset.get_ratings()
+    return ratings[ratings.index.get_level_values('userId') == user_id]['rating'].mean()
+
+
 def predict(
         user_id: int,
         movie_id: int,
         dataset_name: str = 'ml-latest-small',
         treshold_itemsets: float = 0.01,
         treshold_rules: float = 0.01,
-        weighted_mean_metric: str = 'confidence'
+        weighted_mean_metric: str = 'confidence',
+        alpha: float = 0.5,
+        beta: float = 0.5
     ) -> float:
 
     assert weighted_mean_metric in ('antecedent support', 'consequent support', 'support', 'confidence', 'lift')
@@ -23,7 +32,7 @@ def predict(
     prepr = pd.get_dummies(dataset.get_ratings().join(preprocessing.movies_ohe()).drop('timestamp', axis=1).droplevel('movieId'), columns=['rating']).astype(bool)
     user_prepr = prepr[prepr.index == user_id]
 
-    frequent_itemsets = fpgrowth(user_prepr, min_support=treshold_itemsets, use_colnames=True)
+    frequent_itemsets = apriori(user_prepr, min_support=treshold_itemsets, use_colnames=True)
     rules = association_rules(frequent_itemsets, metric="support", min_threshold=treshold_rules)
     rules = rules[rules['consequents'].apply(lambda x: len(x) == 1 and 'rating' in list(x)[0])]
 
@@ -34,14 +43,12 @@ def predict(
     relevant_rules.loc[:, 'consequents'] = relevant_rules['consequents'].apply(lambda x: float(list(x)[0].split('_')[1]))
     prediction = (relevant_rules['consequents'] * relevant_rules[weighted_mean_metric]).sum() / relevant_rules[weighted_mean_metric].sum()
 
-    return prediction if not pd.isna(prediction) else 3.5
+    baseline_prediction = baseline_predictor(user_id, movie_id, dataset_name, alpha)
 
+    if pd.isna(prediction):
+        prediction = baseline_prediction
 
-def baseline_predictor(user_id, movie_id, dataset_name = 'ml-latest-small') -> float:
-    dataset = MovieLensDataset(dataset_name)
-    dataset.delete_rating(user_id, movie_id)
-    ratings = dataset.get_ratings()
-    return ratings[ratings.index.get_level_values('userId') == user_id]['rating'].mean()
+    return prediction * beta + baseline_prediction * (1 - beta)
 
 
 def main() -> None:
