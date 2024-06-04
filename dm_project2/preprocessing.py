d34bf0accd644086255d661d316d87b884488f03
MichalRedm <45372892+MichalRedm@users.noreply.github.com>
2024-06-04 22:37:10 +0200

    Remove unnecessary code from preprocessor

--- dm_project2/preprocessing.py
+++ dm_project2/preprocessing.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from dataset import MovieLensDataset
-from sklearn.preprocessing import StandardScaler
 from sklearn.base import TransformerMixin, BaseEstimator
     
 
@@ -23,10 +22,6 @@ class MovieLensDatasetPreprocessor(BaseEstimator, TransformerMixin):
     
     def movies_ohe(self) -> pd.DataFrame:
         return self._dataset.get_movies()["genres"].str.get_dummies("|").drop(columns="(no genres listed)")    
-
-    def preprocess_movies(self) -> pd.DataFrame:
-        movies_ohe = self.movies_ohe()
-        return pd.DataFrame(StandardScaler().fit_transform(movies_ohe), columns=movies_ohe.columns, index=movies_ohe.index)
     
     def preprocess_ratings(self) -> pd.DataFrame:
         return pd.get_dummies(self._dataset.get_ratings().join(self.movies_ohe()).drop('timestamp', axis=1).droplevel('movieId'), columns=['rating']).astype(bool)
